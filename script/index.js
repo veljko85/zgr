72c511877a9031c9d3a12466e3e05aec65f889f7
Velibor Markovic <veljko85@users.noreply.github.com>
2022-08-08 13:49:38 +0200

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -52,11 +52,15 @@ function scrollVideo(videoNum, nextScrollSec) {
     videos[videoNum].style.opacity = 1;
     for (let i = 0; i < videosRev.length; i++) {
       videosRev[i].style.opacity = 0;
+      videosRev[i].currentTime = 0;
     }
     videos[videoNum].addEventListener("ended", () => {
       scrollSection = nextScrollSec;
       for (let i = 0; i < videos.length; i++) {
-        if (i != videoNum) videos[i].style.opacity = 0;
+        if (i != videoNum) {
+          videos[i].style.opacity = 0;
+          videos[i].currentTime = 0;
+        }
       }
       triger = false;
     });
@@ -69,11 +73,15 @@ function scrollVideoRev(videoNum, prevScrollSec) {
     videosRev[videoNum].style.opacity = 1;
     for (let i = 0; i < videos.length; i++) {
       videos[i].style.opacity = 0;
+      videos[i].currentTime = 0;
     }
     videosRev[videoNum].addEventListener("ended", () => {
       scrollSection = prevScrollSec;
       for (let i = 0; i < videosRev.length; i++) {
-        if (i != videoNum) videosRev[i].style.opacity = 0;
+        if (i != videoNum) {
+          videosRev[i].style.opacity = 0;
+          videosRev[i].currentTime = 0;
+        }
       }
       triger = false;
     });
@@ -84,7 +92,7 @@ function scrollVideoRev(videoNum, prevScrollSec) {
 let videos = document.getElementsByClassName("videos");
 let videosRev = document.getElementsByClassName("videosRev");
 let intervalPlay = setInterval(() => {
-  console.log(video01.currentTime, scrollSection, window.pageYOffset);
+  console.log(videos[0].currentTime, scrollSection, window.pageYOffset);
   if (videos[0].currentTime > 0.01) {
     clearInterval(intervalPlay);
     videos[0].pause();
@@ -100,13 +108,19 @@ document.addEventListener("scroll", () => {
     if (scrollSection == 1) {
       if (!triger) {
         setDisplayCounter(1, counterLine, 0);
-        videosRev[0].style.opacity = 0;
         videos[0].play();
         videos[0].style.opacity = 1;
+        for (let i = 0; i < videosRev.length; i++) {
+          videosRev[i].style.opacity = 0;
+          videosRev[i].currentTime = 0;
+        }
         videos[0].addEventListener("ended", () => {
           scrollSection = 2;
           for (let i = 0; i < videos.length; i++) {
-            if (i != 0) videos[i].style.opacity = 0;
+            if (i != 0) {
+              videos[i].style.opacity = 0;
+              videos[i].currentTime = 0;
+            }
           }
           triger = false;
         });
@@ -114,87 +128,201 @@ document.addEventListener("scroll", () => {
       }
     }
     if (scrollSection == 2) {
-      scrollVideo(1, 3);
       setDisplayCounter(2, counterLine, 1);
+      scrollVideo(1, 3);
     }
     if (scrollSection == 3) {
-      scrollVideo(2, 4);
       setDisplayCounter(3, counterLine, 2);
+      scrollVideo(2, 4);
     }
     if (scrollSection == 4) {
-      scrollVideo(3, 5);
       setDisplayCounter(4, counterLine, 3);
+      scrollVideo(3, 5);
     }
     if (scrollSection == 5) {
-      scrollVideo(4, 6);
       setDisplayCounter(5, counterLine, 4);
+      scrollVideo(4, 6);
     }
     if (scrollSection == 6) {
-      scrollVideo(5, 7);
       setDisplayCounter(6, counterLine, 5);
+      scrollVideo(5, 7);
     }
     if (scrollSection == 7) {
-      scrollVideo(6, 8);
       setDisplayCounter(7, counterLine, 6);
+      scrollVideo(6, 8);
     }
     if (scrollSection == 8) {
-      scrollVideo(7, 9);
       setDisplayCounter(8, counterLine, 7);
+      scrollVideo(7, 9);
     }
     if (scrollSection == 9) {
-      scrollVideo(8, 10);
       setDisplayCounter(9, counterLine, 8);
+      scrollVideo(8, 10);
     }
     if (scrollSection == 10) {
-      scrollVideo(9, 11);
       setDisplayCounter(10, counterLine, 9);
+      scrollVideo(9, 11);
     }
   } else {
     console.log("up", "kurac");
     if (scrollSection == 2) {
-      scrollVideoRev(9, 1);
       setDisplayCounter(1, counterLine, 0);
+      scrollVideoRev(9, 1);
     }
     if (scrollSection == 3) {
-      scrollVideoRev(8, 2);
       setDisplayCounter(1, counterLine, 0);
+      scrollVideoRev(8, 2);
     }
     if (scrollSection == 4) {
-      scrollVideoRev(7, 3);
       setDisplayCounter(2, counterLine, 1);
+      scrollVideoRev(7, 3);
     }
     if (scrollSection == 5) {
-      scrollVideoRev(6, 4);
       setDisplayCounter(3, counterLine, 2);
+      scrollVideoRev(6, 4);
     }
     if (scrollSection == 6) {
-      scrollVideoRev(5, 5);
       setDisplayCounter(4, counterLine, 3);
+      scrollVideoRev(5, 5);
     }
     if (scrollSection == 7) {
-      scrollVideoRev(4, 6);
       setDisplayCounter(5, counterLine, 4);
+      scrollVideoRev(4, 6);
     }
     if (scrollSection == 8) {
-      scrollVideoRev(3, 7);
       setDisplayCounter(6, counterLine, 5);
+      scrollVideoRev(3, 7);
     }
     if (scrollSection == 9) {
-      scrollVideoRev(2, 8);
       setDisplayCounter(7, counterLine, 6);
+      scrollVideoRev(2, 8);
     }
     if (scrollSection == 10) {
-      scrollVideoRev(1, 9);
       setDisplayCounter(8, counterLine, 7);
+      scrollVideoRev(1, 9);
     }
     if (scrollSection == 11) {
-      scrollVideoRev(0, 10);
       setDisplayCounter(9, counterLine, 8);
+      scrollVideoRev(0, 10);
     }
   }
   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 });
-// setInterval(() => {
-//   console.log(scrollSection);
-//   console.log(triger);
-// }, 100);
+document.getElementById("arrowUpSect").onclick = () => {
+  console.log("upBtn");
+  if (scrollSection == 2) {
+    setDisplayCounter(1, counterLine, 0);
+    scrollVideoRev(9, 1);
+  }
+  if (scrollSection == 3) {
+    setDisplayCounter(1, counterLine, 0);
+    scrollVideoRev(8, 2);
+  }
+  if (scrollSection == 4) {
+    setDisplayCounter(2, counterLine, 1);
+    scrollVideoRev(7, 3);
+  }
+  if (scrollSection == 5) {
+    setDisplayCounter(3, counterLine, 2);
+    scrollVideoRev(6, 4);
+  }
+  if (scrollSection == 6) {
+    setDisplayCounter(4, counterLine, 3);
+    scrollVideoRev(5, 5);
+  }
+  if (scrollSection == 7) {
+    setDisplayCounter(5, counterLine, 4);
+    scrollVideoRev(4, 6);
+  }
+  if (scrollSection == 8) {
+    setDisplayCounter(6, counterLine, 5);
+    scrollVideoRev(3, 7);
+  }
+  if (scrollSection == 9) {
+    setDisplayCounter(7, counterLine, 6);
+    scrollVideoRev(2, 8);
+  }
+  if (scrollSection == 10) {
+    setDisplayCounter(8, counterLine, 7);
+    scrollVideoRev(1, 9);
+  }
+  if (scrollSection == 11) {
+    setDisplayCounter(9, counterLine, 8);
+    scrollVideoRev(0, 10);
+  }
+};
+document.getElementById("arrowDownSect").onclick = () => {
+  console.log("downBtn");
+  if (scrollSection == 1) {
+    if (!triger) {
+      setDisplayCounter(1, counterLine, 0);
+      videos[0].play();
+      videos[0].style.opacity = 1;
+      for (let i = 0; i < videosRev.length; i++) {
+        videosRev[i].style.opacity = 0;
+        videosRev[i].currentTime = 0;
+      }
+      videos[0].addEventListener("ended", () => {
+        scrollSection = 2;
+        for (let i = 0; i < videos.length; i++) {
+          if (i != 0) {
+            videos[i].style.opacity = 0;
+            videos[i].currentTime = 0;
+          }
+        }
+        triger = false;
+      });
+      triger = true;
+    }
+  }
+  if (scrollSection == 2) {
+    setDisplayCounter(2, counterLine, 1);
+    scrollVideo(1, 3);
+  }
+  if (scrollSection == 3) {
+    setDisplayCounter(3, counterLine, 2);
+    scrollVideo(2, 4);
+  }
+  if (scrollSection == 4) {
+    setDisplayCounter(4, counterLine, 3);
+    scrollVideo(3, 5);
+  }
+  if (scrollSection == 5) {
+    setDisplayCounter(5, counterLine, 4);
+    scrollVideo(4, 6);
+  }
+  if (scrollSection == 6) {
+    setDisplayCounter(6, counterLine, 5);
+    scrollVideo(5, 7);
+  }
+  if (scrollSection == 7) {
+    setDisplayCounter(7, counterLine, 6);
+    scrollVideo(6, 8);
+  }
+  if (scrollSection == 8) {
+    setDisplayCounter(8, counterLine, 7);
+    scrollVideo(7, 9);
+  }
+  if (scrollSection == 9) {
+    setDisplayCounter(9, counterLine, 8);
+    scrollVideo(8, 10);
+  }
+  if (scrollSection == 10) {
+    setDisplayCounter(10, counterLine, 9);
+    scrollVideo(9, 11);
+  }
+};
+setInterval(() => {
+  console.log(scrollSection);
+  console.log(triger);
+  console.log(
+    videos[0].currentTime,
+    videos[1].currentTime,
+    videos[2].currentTime
+  );
+  console.log(
+    videosRev[9].currentTime,
+    videosRev[8].currentTime,
+    videosRev[7].currentTime
+  );
+}, 100);
